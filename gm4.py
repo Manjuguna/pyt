1bb9faed4322cdee4f1544c5020ad3ef51e156d5
Manjuguna <52068092+Manjuguna@users.noreply.github.com>
2019-06-27 14:47:03 +0530

    Create gm4.py

---
+++ gm4.py
@@ -0,0 +1,7 @@
+list=[]
+num=int(input("("enter number of elements in list:"))
+for i in range(1,num+1):
+else=int(input("enter elements:"):
+    list1.append(else)
+      print("largest element is:",max(list1))
+  
